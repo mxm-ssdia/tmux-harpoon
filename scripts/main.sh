cc7ccff65a22c6e2eaac9b841fc9cf507ef62971
mxm <mxm.public.dev@gmail.com>
2025-08-22 01:35:41 +0530

    remenber last open + added create sess and window

--- scripts/main.sh
+++ scripts/main.sh
@@ -1,14 +1,22 @@
 #!/usr/bin/env bash
 
-# get all sessions and windows
+STATE_FILE="/tmp/tmux-harpoon-state"
+
+# --- Load sessions ---
 mapfile -t sessions < <(tmux list-sessions -F '#S')
 
-session_index=0
-window_index=0
+# --- Restore last state if exists ---
+if [[ -f "$STATE_FILE" ]]; then
+    read -r session_index window_index <"$STATE_FILE"
+else
+    session_index=0
+    window_index=0
+fi
 
 while true; do
     clear
-    echo "[h/l] move tab  [Ctrl-p/Ctrl-n] move session  "
+    echo "[h/l] move tab  [Ctrl-p/Ctrl-n] move session"
+    echo "[Enter] select  [c] new session  [C] new tab  [r] rename session  [R] rename tab  [q] quit"
     echo
 
     for i in "${!sessions[@]}"; do
@@ -46,17 +54,19 @@ while true; do
         sel_window=$(tmux list-windows -t "$sel_session" -F '#I' | sed -n "$((window_index + 1))p")
         tmux switch-client -t "$sel_session"
         tmux select-window -t "$sel_session:$sel_window"
+        echo "$session_index $window_index" >"$STATE_FILE"
         exit 0
         ;;
-    q) exit 0 ;;
+    q)
+        echo "$session_index $window_index" >"$STATE_FILE"
+        exit 0
+        ;;
     r) # rename session
         echo -n "New session name: "
         read -r newname
         [[ -n "$newname" ]] && tmux rename-session -t "${sessions[$session_index]}" "$newname"
-        # refresh sessions
         mapfile -t sessions < <(tmux list-sessions -F '#S')
         ;;
-
     R) # rename window
         echo -n "New window name: "
         read -r newname
@@ -65,6 +75,16 @@ while true; do
             sel_window=$(tmux list-windows -t "$sel_session" -F '#I' | sed -n "$((window_index + 1))p")
             tmux rename-window -t "$sel_session:$sel_window" "$newname"
         fi
+        ;;
+    c) # create new session
+        echo -n "Session name: "
+        read -r newname
+        [[ -n "$newname" ]] && tmux new-session -d -s "$newname"
+        mapfile -t sessions < <(tmux list-sessions -F '#S')
+        ;;
+    C) # create new window in current session
+        sel_session="${sessions[$session_index]}"
+        tmux new-window -t "$sel_session" -n "new-tab"
         ;;
     esac
 done
